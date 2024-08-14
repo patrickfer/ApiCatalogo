c9b52647c3674223cb116e5f20f286224b0dfb85
patrickfer <patrickferdev@gmail.com>
2024-08-14 00:10:16 -0300

    Add project files.

---
+++ ApiCatalogo/Migrations/20240814030801_AjusteTabelas.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteTabelas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Preco",
+                table: "Produtos",
+                type: "numeric(10,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Produtos",
+                type: "character varying(80)",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemUrl",
+                table: "Produtos",
+                type: "character varying(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "character varying(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Categorias",
+                type: "character varying(80)",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemUrl",
+                table: "Categorias",
+                type: "character varying(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Preco",
+                table: "Produtos",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(10,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Produtos",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(80)",
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemUrl",
+                table: "Produtos",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(300)",
+                oldMaxLength: 300);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(300)",
+                oldMaxLength: 300);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Categorias",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(80)",
+                oldMaxLength: 80);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemUrl",
+                table: "Categorias",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(300)",
+                oldMaxLength: 300);
+        }
+    }
+}
