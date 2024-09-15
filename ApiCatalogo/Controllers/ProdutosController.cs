2659648e3c66763f5681d0d419ae541885759500
patrickfer <patrickferdev@gmail.com>
2024-09-15 20:47:17 -0300

    feat: Page number e page size.

--- ApiCatalogo/Controllers/ProdutosController.cs
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -1,6 +1,7 @@
 ﻿using ApiCatalogo.Context;
 using ApiCatalogo.DTOs;
 using ApiCatalogo.Models;
+using ApiCatalogo.Pagination;
 using ApiCatalogo.Repositories;
 using ApiCatalogo.Repositories.Produtos;
 using AutoMapper;
@@ -69,6 +70,17 @@ namespace ApiCatalogo.Controllers
             }
 
             //var destino = _mapper.Map<Destino>(origem);
+            var produtosDto = _mapper.Map<IEnumerable<ProdutoDTO>>(produtos);
+
+            return Ok(produtosDto);
+        }
+
+        [HttpGet("pagination")]
+        public ActionResult<IEnumerable<ProdutoDTO>> Get([FromQuery]
+                                        ProdutosParameters produtosParameters)
+        {
+            var produtos = _uof.ProdutoRepository.GetProdutos(produtosParameters);
+
             var produtosDto = _mapper.Map<IEnumerable<ProdutoDTO>>(produtos);
 
             return Ok(produtosDto);
--- ApiCatalogo/Repositories/Produtos/IProdutoRepository.cs
+++ ApiCatalogo/Repositories/Produtos/IProdutoRepository.cs
@@ -1,9 +1,17 @@
 ﻿using ApiCatalogo.Models;
+using ApiCatalogo.Pagination;
 
 namespace ApiCatalogo.Repositories.Produtos
 {
     public interface IProdutoRepository : IRepository<Produto>
     {
+        IEnumerable<Produto> GetProdutos(ProdutosParameters produtosParams)
+        {
+            return GetAll()
+                .OrderBy(p =>p.Nome)
+                .Skip((produtosParams.PageNumber - 1) * produtosParams.PageSize)
+                .Take(produtosParams.PageSize).ToList();
+        }
         IEnumerable<Produto> GetProdutosPorCategoria(int id);
     }
 }
